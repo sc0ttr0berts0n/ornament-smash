f0120b285afe212293061550b0cc98cab4b90b14
Scott Robertson <scott.t.robertson@gmail.com>
2020-12-07 17:12:47 -0500

    added no-js support

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -14,3 +14,5 @@ import Game from './game';
 const canvas = document.getElementById('game') as HTMLCanvasElement;
 const game = new Game(canvas);
 window.game = game;
+
+document.body.classList.remove('no-js');
